fc04259bc9c37153cf47c263220193ccd04f167f
Renato Santos <renatops1991@gmail.com>
2021-04-29 19:43:01 -0300

    Chat System with websocket

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.get("/users", (request, response) => {
+    return response.json({
+        message: "Hello World!"
+    });
+});
+
+app.post("/user", (request, response) => {
+    return response.json({
+        message: "User Created Successfully"
+    });
+});
+
+app.listen(3333, () => console.log('Server is running =)'));
